42c7b3dde66c454c81ea1c29b9ffa08b3f2fb579
jean elossy <jean.elossy@yahoo.com>
2021-07-25 19:43:19 +0200

    modif du js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,14 @@ function OpenClose(){
   btnOpenClose.classList.toggle("open__close");
   document.querySelector("nav").classList.toggle("nav");
   document.querySelectorAll(".title").forEach(element => element.classList.toggle("title__return"));
-  document.querySelectorAll("li a").forEach(link => link.style.width = "245px");
   document.querySelector(".fa-angle-double-right").classList.toggle("rotate__icon");
+  widthSelector();
 }
 
-
-
+function widthSelector(){
+  if(document.querySelector("nav").style.width === "6%"){
+    document.querySelectorAll("li a").forEach(link => link.style.width = "75px");
+  }else{
+    document.querySelectorAll("li a").forEach(link => link.style.width = "245px");
+  }
+}
