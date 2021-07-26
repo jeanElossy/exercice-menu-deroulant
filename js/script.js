e6fef4ed930e03d7ed4882428ae84a0e385a6bfe
jean elossy <jean.elossy@yahoo.com>
2021-07-26 20:01:08 +0200

    fin du projet

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 const btnOpenClose = document.querySelector(".open__close__btn");
 
+document.querySelector(".link").focus();
+
 btnOpenClose.addEventListener("click", OpenClose);
 function OpenClose(){
   btnOpenClose.classList.toggle("open__close");
   document.querySelector("nav").classList.toggle("nav");
   document.querySelectorAll(".title").forEach(element => element.classList.toggle("title__return"));
   document.querySelector(".fa-angle-double-right").classList.toggle("rotate__icon");
+  Active();
   widthSelector();
 }
 
 function widthSelector(){
-  if(document.querySelector("nav").style.width === "6%"){
+  if(document.querySelector("nav").style.width === "70px"){
     document.querySelectorAll(".link").forEach(link => link.style.width = "60px");
   }else{
-    document.querySelectorAll(".link").forEach(link => link.style.width = "220px");
+    document.querySelectorAll(".link").forEach(link => link.style.width = "245px");
   }
-  
+}
+
+function Active(){
+  document.querySelector(".link").focus();
 }
